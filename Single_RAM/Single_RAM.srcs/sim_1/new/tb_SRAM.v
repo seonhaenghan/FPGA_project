b093830968bc752468a05797dce517810c2c01e5
seonhaeng.han <zaqzse55@gmail.com>
2022-10-17 15:06:19 +0900

    1017-5 MicroBlaze add

--- Single_RAM/Single_RAM.srcs/sim_1/new/tb_SRAM.v
+++ Single_RAM/Single_RAM.srcs/sim_1/new/tb_SRAM.v
@@ -4,11 +4,55 @@ module tb_SRAM();
 
     reg i_clk = 0;
     reg i_ce;
-    reg i_we;  // write
+    reg i_rwe;  // write
     reg [5:0] i_addr;
-    reg [7:0] i_data;
-    wire [7:0] o_data;
+    reg [7:0] io_data;
 
+    StaticRam dut(
+    .i_clk(i_clk),
+    .i_ce(i_ce),
+    .i_rwe(i_rwe),  // read/write >> 1 / 0
+    .i_addr(i_addr),
+    .io_data(io_data)
+    );
+
+
+     always #5 i_clk = ~i_clk;
+
+    initial begin
+         i_clk = 0; 
+          i_ce = 0;
+          i_we = 0;
+        i_addr = 0;
+        io_data = 0;
+
+        #20 i_ce = 1; i_we = 1;// i_addr = 0; i_data = 10;
+        // #20 i_ce = 1; i_we = 1; i_addr = 1; i_data = 20;
+        // #20 i_ce = 1; i_we = 1; i_addr = 3; i_data = 30;
+        // #20 i_ce = 1; i_we = 1; i_addr = 4; i_data = 40;
+        // #20 i_ce = 1; i_we = 1; i_addr = 5; i_data = 50;
+        // #20 i_ce = 1; i_we = 1; i_addr = 10; i_data = 100;
+        // #20 i_ce = 1; i_we = 1; i_addr = 20; i_data = 200;
+        // #20 i_ce = 1; i_we = 1; i_addr = 25; i_data = 250;
+
+    for (integer i=0; i<64; i = i+1) begin
+        #20 i_addr = i; i_data = i+1;
+    end
+
+    #20 i_we = 0;
+
+    for (integer i=0; i<64; i = i+1) begin
+        #20 i_we = 0; i_addr = 1;
+    end
+
+    #20 $finish;
+
+    end
+
+
+
+    //wire [7:0] o_data;
+/*
     singleRam dut(
     .i_clk(i_clk),
     .i_ce(i_ce),
@@ -49,7 +93,7 @@ module tb_SRAM();
     #20 $finish;
 
     end
-
+*/
 
 
 endmodule
--- Single_RAM/Single_RAM.srcs/sources_1/new/StaticRam.v
+++ Single_RAM/Single_RAM.srcs/sources_1/new/StaticRam.v
@@ -3,5 +3,30 @@
 
 module StaticRam(
 
+    input i_clk,
+    input i_ce,
+    input i_rw,  // read/write >> 1 / 0
+    input [5:0] i_addr,
+    input [7:0] io_data
+
     );
+
+    reg [7:0] mem[0:63];
+    reg [5:0] r_addr;
+
+    assign io_data = (i_rw == 1) ? mem[r_addr] : {8{z}}; // high impedence >> read
+
+    always @(posedge i_clk) begin
+        if(i_ce) begin
+            if(!i_rw) begin
+                mem[i_addr] <= io_data; 
+            end
+            else begin
+               r_addr <= i_addr; 
+            end
+        end
+        else begin   
+        end
+    end
+
 endmodule
