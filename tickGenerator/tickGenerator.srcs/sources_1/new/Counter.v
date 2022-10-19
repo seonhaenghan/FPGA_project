4bd5710f6b656ecc56d4298423505133f151b30b
seonhaeng.han <zaqzse55@gmail.com>
2022-10-19 11:37:41 +0900

    1019-2 tickGenerator ip  add

--- tickGenerator/tickGenerator.srcs/sources_1/new/Counter.v
+++ tickGenerator/tickGenerator.srcs/sources_1/new/Counter.v
@@ -1,26 +1,21 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2022/10/19 11:16:47
-// Design Name: 
-// Module Name: Counter
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module Counter(
-
+    input i_clk,
+    input i_reset,
+    output [31:0] o_counter
     );
+
+    reg [31:0] r_counter = 0;
+    assign o_counter = r_counter;
+
+    always @(posedge i_clk, posedge i_reset) begin
+        if(i_reset) begin
+            r_counter <= 0;
+        end
+        else begin
+            r_counter <= r_counter + 1; 
+        end
+    end
+
 endmodule
